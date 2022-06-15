2b8727327d7429a777d8c94ce34cbbd3bf5f7c96
Jeffrey <2159118@talnet.nl>
2022-06-15 09:30:04 +0200

    script

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -16,8 +16,19 @@ public class Playermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 pos = transform.position;
+
+        pos.x += h * speed * Time.deltaTime;
+        pos.y += v * speed * Time.deltaTime;
+
+        transform.position = pos;
+
+
+            }
 
 
 } //class
