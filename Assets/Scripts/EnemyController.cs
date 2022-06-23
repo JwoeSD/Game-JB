224c9873d6c06cf624ee27368bbc46d188772212
BurnedLeaf <2154897@talnet.nl>
2022-06-23 13:51:49 +0200

    finished AI script

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,33 +4,61 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
-    public Transform target;
-    public float minimumDistance;
+    [SerializeField]
+    Transform player;
+    [SerializeField]
+    float agroRange;
+    [SerializeField]
+    float moveSpeed;
 
-
-
-    
+    Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-    
-        
+
+        rb2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     private void Update()
-
-
     {
-        float dist = Vector2.Distance(target.position, transform.position);
-        if (Vector2.Distance(transform.position, target.position) > minimumDistance)
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+ 
+        if(distToPlayer < agroRange) 
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasingPlayer();
+        }
+
+
+
+    }
+    private void ChasePlayer()
+    {
+        if (transform.position.x < player.position.x)
+        {
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(-1, 1);
+
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+
         }
     }
 
+    private void StopChasingPlayer()
+    {
+        rb2d.velocity = new Vector2(0, 0);
+    }
 }
+
 
 
